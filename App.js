f65690baa967415b1e839caf0cd18a7095b71f15
KhushnudEshtemirov <khushnudeshtemirov@gmail.com>
2023-10-25 17:43:00 +0500

    Updated

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World!</Text>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World!</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: "blue",
+  },
+  text: {
+    color: "red",
+  },
+});
 
 export default App;
